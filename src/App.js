6e21e2b0d61a4922cd54310e463de32ce4ad7028
Pikin <pikin@bk.ru>
2024-11-08 17:48:15 +0500

    Header started

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Profile from './components/Profile';
 
+
+
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header children={
+        <>
+
+        </>
+      } />
       <Profile />
       <Footer />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,18 @@
+
+const UserContext = createContext({ name: 'Guest' });
+const ThemeContext = createContext({ theme: 'light' })
+const userContext = useContext(UserContext);
+const themeContext = useContext(ThemeContext);
+
+
+
 function Header() {
     return ( 
-        <></>
+        
+        <UserContext.Provider value='Guest'>
+            
+        </UserContext.Provider>
+        
      );
 }
 
